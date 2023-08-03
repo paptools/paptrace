a3ae2a0991defd6ff324e03e5bd858c5111849ce
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-08-02 23:09:37 -0400

    Added support for local gtest.

--- cmake/FindGoogleTest.cmake
+++ cmake/FindGoogleTest.cmake
@@ -1,17 +1,26 @@
 # Copyright (c) 2023-present, Ian Dinwoodie.
 # Distributed under the MIT License (http://opensource.org/licenses/MIT).
 
-set(gtest_version "1.12.1")
-message(STATUS "Fetching GoogleTest ${test_version}")
+message(STATUS "Finding GoogleTest")
 
-include(FetchContent)
+#find_package(GTest QUIET)
+find_package(GTest)
+if (NOT GTest_FOUND)
+  message(STATUS "GoogleTest not found, fetching from source")
 
-FetchContent_Declare(gtest
-  URL "https://github.com/google/googletest/archive/refs/tags/release-${gtest_version}.tar.gz"
-)
+  include(FetchContent)
+  set(gtest_version "1.12.1")
+  FetchContent_Declare(gtest_content
+    URL "https://github.com/google/googletest/archive/refs/tags/release-${gtest_version}.tar.gz"
+  )
 
-# For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(gtest)
+  # For Windows: Prevent overriding the parent project's compiler/linker settings
+  set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+  FetchContent_MakeAvailable(gtest_content)
+
+  if (PAPTRACE_INSTALL)
+    list(APPEND install_targets gtest)
+  endif ()
+endif ()
 
 enable_testing()
